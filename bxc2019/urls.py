3ac2d3ea52f0d391522d1f2d63becb4757d8c2ba
lli <leo.julian.li@fokus.fraunhofer.de>
2019-05-14 17:57:02 +0200

    run

--- bxc2019/urls.py
+++ bxc2019/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import example
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', example.urls)
 ]
